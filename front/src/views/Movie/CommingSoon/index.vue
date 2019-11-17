64f044af637ad51422c187878ce83ddfe800ef83
Zhang-tree[! <1071066290@qq.com>
2019-11-18 07:55:04 +0800

    详情页路由传参

--- front/src/views/Movie/CommingSoon/index.vue
+++ front/src/views/Movie/CommingSoon/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="comming">
-    <h1 ref="h" v-bind:style="bgcolor">后台工程师正在努力中 - - - - </h1>
+    <h1 ref="h" v-bind:style="bgcolor"> </h1>
   </div>
 </template>
 
@@ -30,7 +30,6 @@ export default {
     background:url('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1573898810231&di=a3e58dd92730a720fdae40d8b48c1c2a&imgtype=0&src=http%3A%2F%2Fpic2.zhimg.com%2Fv2-cc89624b3c49c10106fd7fb3bbd0690d_b.gif') center;
     background-size: over;
     h1{
-      padding: 10px;
       background: #faf;
     }
   }
